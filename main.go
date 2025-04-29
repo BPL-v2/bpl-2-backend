16bcc3b9b99edaaa6f5ebf3b21cccfa8067a4392
Liberatorist <Liberatorist@gmail.com>
2025-04-29 22:36:13 +0200

    add main frontend to cors domains

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"bpl/config"
 	"bpl/controller"
 	"bpl/docs"
+	"bpl/repository"
+	"fmt"
 	"log"
 	"os"
 	"regexp"
@@ -17,6 +19,7 @@ import (
 	swaggerfiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 	ginprometheus "github.com/zsais/go-gin-prometheus"
+	"gorm.io/gorm"
 )
 
 // @title           BPL Backend API
@@ -29,8 +32,8 @@ import (
 // @securityDefinitions.apikey BearerAuth
 // @in header
 // @name Authorization
-
 func main() {
+	t := time.Now()
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
@@ -46,34 +49,38 @@ func main() {
 		panic(err)
 	}
 	_ = db
-	// err = db.AutoMigrate(
-	// 	&repository.ScoringCategory{},
-	// 	&repository.Objective{},
-	// 	&repository.Condition{},
-	// 	&repository.Event{},
-	// 	&repository.Team{},
-	// 	&repository.User{},
-	// 	&repository.TeamUser{},
-	// 	&repository.StashChange{},
-	// 	&repository.ObjectiveMatch{},
-	// 	&repository.Submission{},
-	// 	&repository.ClientCredentials{},
-	// 	&repository.Signup{},
-	// 	&repository.Oauth{},
-	// 	&repository.KafkaConsumer{},
-	// 	&repository.RecurringJob{},
-	// 	&repository.LadderEntry{},
-	// 	&repository.Character{},
-	// 	&repository.Atlas{},
-	// 	&repository.TeamSuggestion{},
-	// )
-
-	// if err != nil {
-	// 	panic(err)
-	// }
+	// autoMigrate(db)
 
 	r := gin.Default()
-	r.Use(gin.Recovery())
+	r.SetTrustedProxies(nil)
+	addMetrics(r)
+	addDocs(r)
+	setCors(r)
+
+	controller.SetRoutes(r)
+	fmt.Println("Server started in", time.Since(t))
+	r.Run(":8000")
+}
+
+func addMetrics(r *gin.Engine) {
+	p := ginprometheus.NewPrometheus("gin")
+	re := regexp.MustCompile(`\d+`)
+	p.ReqCntURLLabelMappingFn = func(c *gin.Context) string {
+		url := strings.Split(c.Request.URL.String(), "?")[0]
+		url = strings.ReplaceAll(url, "current", "?")
+		url = re.ReplaceAllString(url, "?")
+		return strings.TrimPrefix(url, "/api")
+	}
+	p.MetricsPath = "/api/metrics"
+	p.Use(r)
+}
+
+func addDocs(r *gin.Engine) {
+	docs.SwaggerInfo.BasePath = "/api"
+	r.GET("/api/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+}
+
+func setCors(r *gin.Engine) {
 	corsConfigGetOptions := cors.Config{
 		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "OPTIONS"},
@@ -83,6 +90,7 @@ func main() {
 	}
 	corsConfigOtherMethods := cors.Config{
 		AllowOrigins: []string{
+			"https://bpl-poe.com/",
 			"https://bpl-2.netlify.app",
 			"https://v2202503259898322516.goodsrv.de",
 			"http://localhost",
@@ -113,28 +121,28 @@ func main() {
 			cors.New(corsConfigOtherMethods)(c)
 		}
 	})
-
-	addMetrics(r)
-	addDocs(r)
-
-	controller.SetRoutes(r)
-	r.Run(":8000")
 }
 
-func addMetrics(r *gin.Engine) {
-	p := ginprometheus.NewPrometheus("gin")
-	re := regexp.MustCompile(`\d+`)
-	p.ReqCntURLLabelMappingFn = func(c *gin.Context) string {
-		url := strings.Split(c.Request.URL.String(), "?")[0]
-		url = strings.ReplaceAll(url, "current", "?")
-		url = re.ReplaceAllString(url, "?")
-		return strings.TrimPrefix(url, "/api")
+func autoMigrate(db *gorm.DB) {
+	err := db.AutoMigrate(
+		&repository.ScoringCategory{},
+		&repository.Objective{},
+		&repository.Condition{},
+		&repository.Event{},
+		&repository.Team{},
+		&repository.User{},
+		&repository.TeamUser{},
+		&repository.StashChange{},
+		&repository.ObjectiveMatch{},
+		&repository.Submission{},
+		&repository.ClientCredentials{},
+		&repository.Signup{},
+		&repository.Oauth{},
+		&repository.KafkaConsumer{},
+		&repository.RecurringJob{},
+		&repository.LadderEntry{},
+	)
+	if err != nil {
+		panic(err)
 	}
-	p.MetricsPath = "/api/metrics"
-	p.Use(r)
-}
-
-func addDocs(r *gin.Engine) {
-	docs.SwaggerInfo.BasePath = "/api"
-	r.GET("/api/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 }
