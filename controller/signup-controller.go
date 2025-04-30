962c71ef90f53a676ce3bd20b6da5b8533412b49
Liberatorist <Liberatorist@gmail.com>
2025-04-30 07:05:07 +0200

    disable discord server check for now

--- controller/signup-controller.go
+++ controller/signup-controller.go
@@ -91,11 +91,12 @@ func (e *SignupController) createSignupHandler() gin.HandlerFunc {
 			c.JSON(401, gin.H{"error": "Not authenticated"})
 			return
 		}
-		err = e.userService.DiscordServerCheck(user)
-		if err != nil {
-			c.JSON(403, gin.H{"error": err.Error()})
-			return
-		}
+
+		// err = e.userService.DiscordServerCheck(user)
+		// if err != nil {
+		// 	c.JSON(403, gin.H{"error": err.Error()})
+		// 	return
+		// }
 		var signupCreate SignupCreate
 		if err := c.BindJSON(&signupCreate); err != nil {
 			c.JSON(400, gin.H{"error": err.Error()})
