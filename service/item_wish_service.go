dc182aa4443f927246d4db4beb45393f272b6895
Liberatorist <fabian.mueller77@gmail.com>
2025-12-23 15:35:30 +0100

    .

--- service/item_wish_service.go
+++ service/item_wish_service.go
@@ -39,7 +39,7 @@ func (s *ItemWishService) UpdateItemWish(itemWish *repository.ItemWish, teamId i
 		if err != nil {
 			return nil, err
 		}
-		priority := int(math.Max(math.Min(float64(*Priority), float64(len(itemWishes))), 0))
+		priority := int(math.Max(math.Min(float64(*Priority), float64(len(itemWishes)-1)), 0))
 		for _, iw := range itemWishes {
 			if iw.Priority == priority {
 				iw.Priority = itemWish.Priority
