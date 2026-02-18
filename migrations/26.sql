8b66e3294534200076e0a60189705571460bccc9
Liberatorist <fabian.mueller77@gmail.com>
2026-02-18 01:46:17 +0100

    fix migration

--- migrations/26.sql
+++ migrations/26.sql
@@ -1,3 +1,14 @@
+ALTER TABLE guilds ADD COLUMN event_id INT;
+
+UPDATE guilds g
+SET event_id = (SELECT t.event_id
+                FROM teams t
+                WHERE t.id = g.team_id);
+
 ALTER TABLE guilds
-ADD COLUMN event_id INT NOT NULL,
-ADD CONSTRAINT fk_guilds_event_id FOREIGN KEY (event_id) REFERENCES events(id) ON DELETE CASCADE;
+ADD CONSTRAINT fk_guilds_event_id
+FOREIGN KEY (event_id) REFERENCES events(id)
+ON DELETE CASCADE;
+
+ALTER TABLE guilds
+ALTER COLUMN event_id SET NOT NULL;
